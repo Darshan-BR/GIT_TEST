f9bea9da1a7e77fb179c3cf0a262760041434228
Darshan <darshan08.mohole@gmail.com>
2025-07-06 00:04:51 +0530

    New PGM

---
+++ T1.py
@@ -0,0 +1,9 @@
+# Ask the user for width and height
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+
+# Calculate the area
+area = width * height
+
+# Display the result
+print("The area of the rectangle is:", area)
