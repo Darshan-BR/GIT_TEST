d236b2bf11215e8cbb495779d4b875a14d602acb
Darshan-BR <darshan08.mohole@gmail.com>
2025-07-06 00:13:53 +0530

    Update T1.py

--- T1.py
+++ T1.py
@@ -1,4 +1,4 @@
-# Ask the user for width and height
+# Ask the user for width and height for Calculate the area
 width = float(input("Enter the width of the rectangle: "))
 height = float(input("Enter the height of the rectangle: "))
 
